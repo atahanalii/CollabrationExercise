36e1a88de79dd483a6aaf35c99134dcf1a58e183
atahanalii <atahanalii@gmail.com>
2014-10-06 13:28:56 +0200

    new

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabrationExercise
+{
+    internal class Program
+    {
+        /// <summary>
+        /// this is the main  methood
+        /// it receives a input from commandline
+        /// and checks if it number or letter
+        /// it will then check if it is a palidrome 
+        /// if it is a number it will present the same 
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+           
+        }
+
+        private static bool IsPalindrome(String text)
+        {
+        }
+
+        private static bool ContainsOnlyDigits(String text)
+        {
+        }
+        private static string ConvertToBinaryNumbers(int input)
+        {
+        }
+        private static string ConvertToRomanNumbers(int input)
+        {
+        }
+    }
+}
